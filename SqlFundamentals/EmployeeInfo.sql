1447332c27d65fe6fca62c1bd68005efb4099520
srdrflk <55311271+srdrflk@users.noreply.github.com>
2025-01-15 13:37:21 +0300

    TASK3 Stored procedure added to project

---
+++ SqlFundamentals/EmployeeInfo.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+AS
+SELECT TOP 1000
+    E.Id AS EmployeeId,
+    ISNULL(E.EmployeeName, P.FirstName + ' ' + P.LastName) AS EmployeeFullName,
+    A.ZipCode + '_' + A.State + ', ' + A.City + '-' + A.Street AS EmployeeFullAddress,
+    E.CompanyName + '(' + ISNULL(E.Position, '') + ')' AS EmployeeCompanyInfo
+FROM 
+    Employee E
+INNER JOIN 
+    Person P ON E.PersonId = P.Id
+INNER JOIN 
+    Address A ON E.AddressId = A.Id
+ORDER BY 
+    E.CompanyName ASC, 
+    A.City ASC;
+
+
+-- NOTE:  to be able to use order by at view, we need to select TOP ... 
---
+++ SqlFundamentals/InsertEmployee.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [dbo].[InsertEmployee]
+
+    @EmployeeName NVARCHAR(100) = NULL,
+    @FirstName NVARCHAR(50) = NULL,
+    @LastName NVARCHAR(50) = NULL,
+    @CompanyName NVARCHAR(20),
+    @Position NVARCHAR(30) = NULL,
+    @Street NVARCHAR(50),
+    @City NVARCHAR(20) = NULL,
+    @State NVARCHAR(50) = NULL,
+    @ZipCode NVARCHAR(50) = NULL
+AS
+BEGIN
+
+    IF LTRIM(RTRIM(ISNULL(@EmployeeName, ''))) = '' 
+        AND LTRIM(RTRIM(ISNULL(@FirstName, ''))) = '' 
+        AND LTRIM(RTRIM(ISNULL(@LastName, ''))) = ''
+    BEGIN
+        RAISERROR('At least one name field must be provided.', 16, 1);
+        RETURN;
+    END
+
+    -- Truncate CompanyName if it's longer than 20 characters
+    SET @CompanyName = LEFT(@CompanyName, 20);
+
+    DECLARE @AddressId INT;
+    INSERT INTO Address (Street, City, State, ZipCode)
+    VALUES (@Street, @City, @State, @ZipCode);
+    
+    SELECT @AddressId = SCOPE_IDENTITY();
+
+    DECLARE @PersonId INT = NULL;
+    IF LTRIM(RTRIM(ISNULL(@FirstName, ''))) <> '' OR LTRIM(RTRIM(ISNULL(@LastName, ''))) <> ''
+    BEGIN
+        INSERT INTO Person (FirstName, LastName)
+        VALUES (@FirstName, @LastName);
+        
+        SELECT @PersonId = SCOPE_IDENTITY();
+    END
+
+    INSERT INTO Employee (AddressId, PersonId, CompanyName, Position, EmployeeName)
+    VALUES (@AddressId, @PersonId, @CompanyName, @Position, @EmployeeName);
+END
